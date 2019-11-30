ea036988d1b8f1313b775e34fa611de843f23763
Horst Jahns <horst.jahns@auto1.com>
2019-11-30 18:41:17 +0100

    v1.1.0 adding table header with accumulated info

--- src/components/LogTable.vue
+++ src/components/LogTable.vue
@@ -1,29 +1,37 @@
 <template>
-  <v-simple-table>
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left">Date</th>
-          <th class="text-left">Action</th>
-          <th class="text-left">Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="log in logs" :key="log._id">
-          <td>{{ log.date }}</td>
-          <td>{{ log.action }}</td>
-          <td>{{ log.status.status }}</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <v-row>
+    <v-col cols="3">Trades: {{ countTrades }}</v-col>
+    <v-col cols="3">Success Rate</v-col>
+    <v-col cols="3">Profit per Trade</v-col>
+    <v-col cols="3">Profit absolute</v-col>
+    <v-col cols="12">
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">Date</th>
+              <th class="text-left">Action</th>
+              <th class="text-left">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="log in logs" :key="log._id">
+              <td>{{ log.date }}</td>
+              <td>{{ log.action }}</td>
+              <td>{{ log.status.status }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import StorageService from '../service/StorageService';
+import StorageService from "../service/StorageService";
 
 export default {
-  name: 'LogTable',
+  name: "LogTable",
 
   data: () => ({
     logs: []
@@ -33,9 +41,20 @@ export default {
     this.updateLogs();
   },
 
+  computed: {
+    countTrades: function() {
+      return this.logs.filter(log => {
+        return (
+          log.status.hasOwnProperty("performance") &&
+          log.status.performance !== 0
+        );
+      }).length;
+    }
+  },
+
   methods: {
     updateLogs: function() {
-      StorageService.getJsonStorage().then((response) => {
+      StorageService.getJsonStorage().then(response => {
         this.logs = response.data.logs.reverse();
       });
     }
@@ -44,7 +63,8 @@ export default {
 </script>
 
 <style scoped>
-th.text-left, td {
+th.text-left,
+td {
   text-align: left;
 }
 </style>
